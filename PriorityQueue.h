44bbd8974e4051c8836f77c7e5062a240df138bf
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-04-05 19:41:23 +0200

    separated generate Huff Tree to a separate function

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "BinNode.h"
 #include <stdio.h>
 #include <stdlib.h>
---
+++ generateHuffTree.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "BinNode.h"
+#include "PriorityQueue.h"
+
+BinNode *generateHuffTree(PriorityQueue *freqsQ) {
+    bool underflow = false;
+
+    while (true) {
+        Box *firstElem = freqsQ->deQueue(&underflow);
+        Box *secElem = freqsQ->deQueue(&underflow);
+
+        if (underflow) return firstElem->node;
+
+        int freqTotal = firstElem->node->freqs + secElem->node->freqs;
+
+        char *letters = new char[strlen(firstElem->node->letters) + strlen(secElem->node->letters) + 1];
+        letters = strcpy(letters, firstElem->node->letters);
+        strcat(letters, secElem->node->letters);
+
+        BinNode *node = new BinNode(letters, freqTotal);
+        if (firstElem->node->freqs <= secElem->node->freqs) {
+            node->left = firstElem->node;
+            node->right = secElem->node;
+        } else {
+            node->left = secElem->node;
+            node->right = firstElem->node;
+        }
+
+        delete firstElem;
+        delete secElem;
+        freqsQ->enQueue(node);
+    }
+}
