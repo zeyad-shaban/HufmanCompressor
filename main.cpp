3be33edb861df97ea739a4cf96c4db4a3762d7ed
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-04-11 07:33:57 +0200

    idk smoe chanes?

--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ int main() {
     freqTable['c'] = 5;
     freqTable['d'] = 1;
 
+    heap->insert("a", 10, &overflow);
+    heap->insert("b", 9, &overflow);
+    heap->insert("b", 9, &overflow);
+    heap->insert("b", 1, &overflow);
+
+    if (overflow)
+        std::cout << "Some values are missimng due to the limited size" << std::endl;
+
+    Node min = heap->extractMin();
+    std::cout << heap->traverse(0) << std::endl;
+
+
     delete heap;
     return 0;
 }
