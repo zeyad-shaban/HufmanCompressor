ee52153e308a4df3ee3b3633d08eabdc8f1089c8
Ahmed Hesham <A.Ahmed06380@student.aast.edu>
2024-04-12 22:04:08 +0200

    saving encoder and decoder in a proper way

--- compress.h
+++ compress.h
@@ -9,7 +9,6 @@
 using namespace std;
 
 class compress {
-	// TODO SAVE ENCODER AND DECODER AND JSON AND LOAD LATER
     map<std::string, std::string> encoder;
     map<std::string, std::string> decoder;
 public:
@@ -25,23 +24,7 @@ public:
         createMaps(root->left, code + "0");
         createMaps(root->right, code + "1");
     }
-private:
-    void saveEncoder(string filename) {
-        string data = "";
-        for (auto it = encoder.begin(); it != encoder.end(); it++) {
-            data += it->first + " " + it->second + "\n";
-        }
-        saveStringToFile((string("./data/") + filename + string(".enc")).c_str(), data.c_str());
-    }
-    void saveDecoder(string filename) {
-        string data = "";
-        for (auto it = decoder.begin(); it != decoder.end(); it++) {
-            data += it->first + " " + it->second + "\n";
-        }
-        saveStringToFile((string("./data/") + filename + string(".dec")).c_str(), data.c_str());
-    }
-
-	string compressing(string filePath, string filename) {
+    	string compressing(string filePath, string filename) {
         string codedText = "";
 		std::fstream file(filePath);
 		if (!file.is_open()) return "";
@@ -68,9 +51,22 @@ private:
 		saveStringToFile((string("./data/decompressed_") + filename + string(".txt")).c_str(), decodedText.c_str());
         return decodedText;
     }
-
-    // printing the encoder table
-    void printEncoder() {
+private:
+    void saveEncoder(string filename) {
+        string data = "Keys | Values\n";
+        for (auto it = encoder.begin(); it != encoder.end(); it++) {
+            data += it->first + " | " + it->second + "\n";
+        }
+        saveStringToFile((string("./data/") + filename + string(".txt")).c_str(), data.c_str());
+    }
+    void saveDecoder(string filename) {
+        string data = "Keys | Values\n";
+        for (auto it = decoder.begin(); it != decoder.end(); it++) {
+            data += it->first + " | " + it->second + "\n";
+        }
+        saveStringToFile((string("./data/") + filename + string(".txt")).c_str(), data.c_str());
+    }
+        void printEncoder() {
         for (auto it = encoder.begin(); it != encoder.end(); it++) {
             cout << it->first << " " << it->second << endl;
         }
