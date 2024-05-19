0ed52c40eabb0fa398c33edfa775735b1f372054
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-05-19 20:50:55 +0300

    i don't reclal what i did but i did some stuff

--- backend/Project1/MainFrame.cpp
+++ backend/Project1/MainFrame.cpp
@@ -9,7 +9,7 @@ void MainFrame::onCompress(wxCommandEvent& event) {
 	Node* root = NULL;
 	string textPrev;
 
-	startCompressing(filePath, dirPath, 100, &root); // TODO TAKE THIS THE COMPRESSION ORDER (2) THROUGH GUI
+	startCompressing(filePath, dirPath, 1, &root); // TODO TAKE THIS THE COMPRESSION ORDER (2) THROUGH GUI
 
 	InfoFrame* infoFrame = new InfoFrame("Info", this, encoder, root, textPrev, true);
 	this->Show(false);
--- backend/Project1/main.cpp
+++ backend/Project1/main.cpp
@@ -13,8 +13,8 @@ using namespace std;
 int main(int argc, char** argv) {
 	bool devMode = true;
 	if (devMode) {
-		std::string dirpath = "C:/Users/zeyad/OneDrive/Desktop/test"; // please do not delete it instead create a new variable to override it if needed 
-		std::string filename = "small";
+		std::string dirpath = "C:/Users/zeyad/OneDrive/Desktop/test/scripts"; // please do not delete it instead create a new variable to override it if needed 
+		std::string filename = "10gbInput";
 		int compressionOrder = 100;
 
 		startCompressing(dirpath + "/" + filename + ".txt", dirpath, compressionOrder);
@@ -34,3 +34,19 @@ int main(int argc, char** argv) {
 	else
 		std::cout << "Invalid input";
 }
+
+
+
+/*
+TODO
+
+Loading bar
+
+Fix win GUI & add in for max order
+
+err handle for missing files and handle if tmp0 or tmp1 is already there and chnage the handle for tmp until we find something that would work
+
+Ability to generate file
+
+Take max compression order
+*/
