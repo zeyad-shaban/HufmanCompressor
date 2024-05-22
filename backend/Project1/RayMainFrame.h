ecf9333c86c584cd50516b3b78d9aad795033379
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-05-22 11:37:56 +0300

    FINALLY RAYGUI IS WORKING!!! THIS IS SO GREAT

--- backend/Project1/RayMainFrame.h
+++ backend/Project1/RayMainFrame.h
@@ -1,6 +1,34 @@
-#pragma once
-#include <iostream>
 #include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-int RayMainFrame();
+int RayMainFrame() {
+    InitWindow(400, 200, "raygui - controls test suite");
+    SetTargetFPS(60);
+
+    bool showMessageBox = false;
+
+    while (!WindowShouldClose())
+    {
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+        if (GuiButton(Rectangle{ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox(Rectangle{ 85, 70, 250, 100 },
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+            if (result >= 0) showMessageBox = false;
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
