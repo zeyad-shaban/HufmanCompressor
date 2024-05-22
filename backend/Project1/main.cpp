03659d6aff8e3eb1bdf456946fac1c445dad76a6
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-05-23 01:09:39 +0300

    main.cpp udpated

--- backend/Project1/main.cpp
+++ backend/Project1/main.cpp
@@ -1,29 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
 #include "RayMainFrame.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-using namespace std;
 
 
 int main() {
 	RayMainFrame();
 }
-
-
-
-/*
-TODO
-
-Loading bar
-
-Fix win GUI & add in for max order
-
-err handle for missing files and handle if tmp0 or tmp1 is already there and chnage the handle for tmp until we find something that would work
-
-Ability to generate file
-
-Take max compression order
-*/
